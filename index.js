eb4a35e61df8fea6b39e61ad0162a136adf2e364
KaranBhakuni <karanbhakuni21@gmail.com>
2025-04-28 01:05:16 +0530

    learned some types and how to use ts in a poject

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+let x = 1;
+// let x = 1;  type inferencing
+x = "karan";
+console.log(x);
+
+
--- index.ts
+++ index.ts
@@ -1,2 +1,51 @@
-let x: number = 1;
+
+//let x: number = 1; //x can store number only 
+let x: number | string= 1; //x can store number and string both
+// let x = 1;  type inferencing
+x= "karan"
 console.log(x)
+
+
+function greet(name: string){
+    console.log(" hi "+ name);
+}
+
+greet("karan");
+
+function greet2(name: any){   // any type can store all types
+    console.log(" hi "+ name);
+}
+greet2("karan");
+greet2(1);
+greet2(true);
+
+
+function sum(a: number,b:number){  
+    return a+b;
+}
+//we dont have to define return type of the 
+// fucntion beacause ts can automatically inferred the return type
+// of the sum function is a number so , the ans is type fo number.. tis called type inferencing
+
+let ans=sum(1,3);
+console.log(ans);
+
+
+function sum2(a: string,b:number){   
+    return a+b;
+}
+//type inferecing : stirng + number = string , so 
+// the type of ans2 is string automatically beacuse return type of 
+//  function is string
+let ans2=sum(1,3);
+console.log(ans2)
+
+
+// explicitlly giving type to a function, so ts compiler doesnt automatically inferr it 
+function sum3(a: number,b:number): number {
+    return a+b;
+}
+
+
+let ans3=sum(3,5);
+console.log(ans3 )
